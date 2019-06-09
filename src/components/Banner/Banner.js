03de1f259e4e02f030cd74c3fdc4143c0418dd8b
Qbisst <ismaelaperez@outlook.com>
2019-06-09 11:23:45 -0400

    created banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => (
+    <div className="banner text-center d-flex align-items-center">
+        <p className="m-0">
+            Click on an image once to earn points. If you click an image more than once, it is game over and the counter will return to zero
+    </p>
+    </div>
+);
+
+export default Banner;
